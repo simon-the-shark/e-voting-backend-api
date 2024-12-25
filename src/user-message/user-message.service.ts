3b77189d56b7217f40f2a36798307da5bebf500b
Szymon Kowaliński <szymon@kowalinski.dev>
2024-12-25 22:06:39 +0100

    feat(user-message): add service && dto

--- src/user-message/user-message.service.ts
+++ src/user-message/user-message.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserMessage } from './entities/user-message.entity';
 
 @Injectable()
-export class UserMessageService {}
+export class UserMessageService {
+  constructor(
+    @InjectRepository(UserMessage)
+    private readonly userMessageRepository: Repository<UserMessage>,
+  ) {}
+  async createMessage(dto: CreateMessageDto) {
+    const newOne = await this.userMessageRepository.create({
+      message: dto.message,
+      isRead: false,
+      user: {
+        id: dto.userId,
+      },
+    });
+    this.userMessageRepository.save(newOne);
+  }
+
+  async markAsRead(id: number) {
+    await this.userMessageRepository.update(id, { isRead: true });
+  }
+
+  async getAllUnread(userId: number) {
+    return this.userMessageRepository.find({
+      where: {
+        user: {
+          id: userId,
+        },
+        isRead: false,
+      },
+    });
+  }
+}
